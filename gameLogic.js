3c0195992cfdb2e01c973ea8ad65045a0ed03572
Benjamin Singer <benjaminjsinger@gmail.com>
2016-12-02 19:05:20 -0600

    changes that didnt help a whole lot

--- gameLogic.js
+++ gameLogic.js
@@ -1,6 +1,6 @@
 /*global console, setInterval, module*/
 
-(function () {
+(function() {
     'use strict';
 
     var clients;
@@ -9,7 +9,7 @@
     var canvasWidth = 1920;
     var canvasHeight = 1080;
 
-    var getCenterAndRadius = function (s) {
+    var getCenterAndRadius = function(s) {
         if (s) {
             var r = {};
             r.x = s.x;
@@ -19,7 +19,7 @@
         }
     };
 
-    var collisionCheck = function (s1, s2) {
+    var collisionCheck = function(s1, s2) {
         try {
             if (s1 && s2 && s1.shape !== 'line' && s2.shape !== 'line') {
                 var crS1 = getCenterAndRadius(s1);
@@ -41,7 +41,7 @@
     };
 
     var uuidV4 = require('uuid/v4');
-    var Circle = function (x, y, r, options) {
+    var Circle = function(x, y, r, options) {
         var circle = {
             id: uuidV4(),
             shape: "circle",
@@ -61,7 +61,27 @@
         return circle;
     };
 
-    var Line = function (x1, y1, x2, y2, options) {
+    // var Rectangle = function(x, y, w, h, options) {
+    //     var rectangle = {
+    //         id: uuidV4(),
+    //         shape: "rectangle",
+    //         x: x,
+    //         w: w,
+    //         h: h,
+    //         color: 'black'
+    //     };
+
+    //     var key;
+    //     for (key in options) {
+    //         if (options[key] !== undefined) {
+    //             rectangle[key] = options[key];
+    //         }
+    //     }
+
+    //     return rectangle;
+    // };
+
+    var Line = function(x1, y1, x2, y2, options) {
         var line = {
             id: uuidV4(),
             shape: "line",
@@ -82,13 +102,13 @@
         return line;
     };
 
-    var rand = function (min, max) {
+    var rand = function(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     };
 
     var entities = {};
 
-    var createAndAddThing = function () {
+    var createAndAddThing = function() {
         var negativeX = [1, -1][Math.round(Math.random())];
         var negativeY = [1, -1][Math.round(Math.random())];
         var newX = Math.random(0, 20);
@@ -102,7 +122,8 @@
         var height = r * 2;
         var thing = new Circle(x, y, r, {
             color: 'blue',
-            beforeUpdate: function () {
+            type: 'thing',
+            beforeUpdate: function() {
                 this.color = 'blue';
                 this.x += vx;
                 this.y += vy;
@@ -110,7 +131,7 @@
                     delete entities[this.id];
                 }
             },
-            onCollision: function (collidedObj) {
+            onCollision: function(collidedObj) {
                 this.color = 'purple';
                 if (collidedObj.type === 'bullet' && collidedObj.playerId !== this.id) {
                     this.isDead = true;
@@ -124,16 +145,16 @@
 
     //create some shapes to shoot every 2 seconds
     var i;
-    for (i = 0; i < 5; i += 1) {
+    for (i = 0; i < 15; i += 1) {
         createAndAddThing();
     }
-    setInterval(function () {
-        createAndAddThing();
-    }, 1000);
+    // setInterval(function() {
+    //     createAndAddThing();
+    // }, 1000);
 
 
 
-    var addNewPlayer = function (ws) {
+    var addNewPlayer = function(ws) {
         //var player = new Circle(rand(0, canvasWidth), rand(0, canvasHeight), 10, {
         var player = new Circle(rand(0, 300), rand(0, 300), 10, {
             type: 'player',
@@ -143,7 +164,7 @@
                 b: rand(100, 200),
                 a: 1
             },
-            beforeUpdate: function () {
+            beforeUpdate: function() {
                 this.healthPercentage = Math.floor((this.health / this.maxHealth) * 100);
                 if (this.timeToReload && Date.now() >= this.timeToReload) {
                     this.reloading = false;
@@ -158,7 +179,43 @@
                 }
                 this.manageGun();
             },
-            onCollision: function (collidedObj) {},
+            onCollision: function(collidedObj) {
+                // if (collidedObj.x > this.x) {
+                //     this.x += 1.1;
+                // } else {
+                //     this.x -= 1.1;
+                // }
+                // if (collidedObj.y > this.y) {
+                //     this.y -= 1.1;
+                // } else {
+                //     this.y += 1.1;
+                // }
+
+                // var cObj = collidedObj;
+                // if (cObj.shape === 'circle' && cObj.type !== 'bullet') {
+                //     var slope = (cObj.y - this.y) / (cObj.x - this.x);
+                //     var k = ((cObj.r + this.r) / 4) / (Math.sqrt(1 + Math.pow(slope, 2)));
+                //     k = Math.ceil(k);
+                //
+                //     console.log(cObj.r, this.r, cObj.r + this.r, Math.ceil(Math.sqrt(Math.pow(k, 2) + Math.pow(slope * k, 2))));
+                //     var newX;
+                //     var newY;
+                //     if (cObj.x < this.x) {
+                //         newX = Math.ceil(this.x + k);
+                //     } else {
+                //         newX = Math.ceil(this.x - k);
+                //     }
+                //     if (cObj.y < this.y) {
+                //         newY = Math.ceil(this.y - (k * slope));
+                //     } else {
+                //         newY = Math.ceil(this.y + (k * slope));
+                //
+                //     }
+                //     // console.log(newX, newY);
+                //     this.x = newX;
+                //     this.y = newY;
+                // }
+            },
             acc: {
                 left: 0,
                 up: 0,
@@ -178,7 +235,7 @@
             reloadTime: 1250,
             reloading: false,
             isDead: false,
-            reload: function () {
+            reload: function() {
                 if (!this.isDead && this.reloading === false && this.ammo < 5) {
                     this.reloading = true;
                 }
@@ -187,7 +244,7 @@
                 x: canvasWidth / 2,
                 y: canvasHeight / 2
             },
-            stopMoving: function () {
+            stopMoving: function() {
                 this.dObj = {
                     up: false,
                     down: false,
@@ -195,7 +252,7 @@
                     right: false
                 };
             },
-            respawn: function () {
+            respawn: function() {
                 if (this.isDead) {
                     this.x = rand(0, canvasWidth);
                     this.y = rand(0, canvasHeight);
@@ -207,7 +264,7 @@
                     this.isDead = false;
                 }
             },
-            fireGun: function (x, y, ws) {
+            fireGun: function(x, y, ws) {
                 var player = this;
                 if (player.ammo > 0 && player.reloading === false && !player.isDead) {
                     player.ammo -= 1;
@@ -221,7 +278,7 @@
                         playerId: player.id,
                         color: 'black',
                         damage: 400,
-                        beforeUpdate: function () {
+                        beforeUpdate: function() {
                             if (!this.timeAlive) {
                                 this.timeAlive = 0;
                             }
@@ -233,19 +290,21 @@
                                 this.y = this.y + vy;
                             }
                         },
-                        onCollision: function (collidedObj) {
-                            if (collidedObj && collidedObj.type === 'player' && collidedObj.id !== this.playerId && !collidedObj.isDead) {
-                                if (collidedObj.health > 0 && (collidedObj.health - this.damage) > 0) {
-                                    collidedObj.health = collidedObj.health - this.damage;
-                                } else {
-                                    collidedObj.health = 0;
-                                    collidedObj.isDead = true;
-                                    collidedObj.deaths += 1;
-                                    if (gunMap[collidedObj.id]) {
-                                        delete entities[gunMap[collidedObj.id].id];
+                        onCollision: function(collidedObj) {
+                            if (collidedObj.id !== this.playerId && collidedObj.shape !== 'line') {
+                                if (collidedObj && collidedObj.type === 'player' && !collidedObj.isDead) {
+                                    if (collidedObj.health > 0 && (collidedObj.health - this.damage) > 0) {
+                                        collidedObj.health = collidedObj.health - this.damage;
+                                    } else {
+                                        collidedObj.health = 0;
+                                        collidedObj.isDead = true;
+                                        collidedObj.deaths += 1;
+                                        if (gunMap[collidedObj.id]) {
+                                            delete entities[gunMap[collidedObj.id].id];
+                                        }
+                                        delete gunMap[collidedObj.id];
+                                        player.kills += 1;
                                     }
-                                    delete gunMap[collidedObj.id];
-                                    player.kills += 1;
                                 }
                                 delete entities[this.id];
                             }
@@ -254,7 +313,7 @@
                     entities[bullet.id] = bullet;
                 }
             },
-            manageGun: function () {
+            manageGun: function() {
                 var player = playerMap[ws.playerId];
                 if (gunMap[ws.playerId]) {
                     var lastGunId = gunMap[ws.playerId].id;
@@ -284,7 +343,8 @@
                 }
             },
             kills: 0,
-            deaths: 0
+            deaths: 0,
+            pps: 4
         });
         entities[player.id] = player;
         playerMap[player.id] = player;
@@ -292,8 +352,8 @@
         return player;
     };
 
-    var updateEntities = function (dt) {
-        var adjustAcc = function (key, val, player) {
+    var updateEntities = function(dt) {
+        var adjustAcc = function(key, val, player) {
             if (player.dObj[key]) {
                 if (player.acc[key] <= 30) {
                     player.acc[key] += val;
@@ -310,23 +370,22 @@
         };
 
         var playerId, player;
-        var pps = 4;
 
         for (playerId in playerMap) {
             if (playerMap[playerId] !== undefined) {
                 player = playerMap[playerId];
-                adjustAcc('left', 3, player);
-                adjustAcc('up', 3, player);
-                adjustAcc('right', 3, player);
-                adjustAcc('down', 3, player);
+                adjustAcc('left', player.pps, player);
+                adjustAcc('up', player.pps, player);
+                adjustAcc('right', player.pps, player);
+                adjustAcc('down', player.pps, player);
 
-                player.x -= (pps * (player.acc.left - player.acc.right) * dt);
-                player.y -= (pps * (player.acc.up - player.acc.down) * dt);
+                player.x -= (player.pps * (player.acc.left - player.acc.right) * dt);
+                player.y -= (player.pps * (player.acc.up - player.acc.down) * dt);
             }
         }
     };
 
-    var update = function (dt) {
+    var update = function(dt) {
         //checkCollisions
         var e;
         for (e in entities) {
@@ -359,9 +418,9 @@
         }
     };
 
-    var render = function () {
+    var render = function() {
         if (clients && clients.length > 0) {
-            clients.forEach(function (ws) {
+            clients.forEach(function(ws) {
                 if (ws.readyState === 1) {
                     ws.send(JSON.stringify({
                         player: playerMap[ws.playerId],
@@ -376,7 +435,7 @@
     //game loop
     var raf = require('raf');
     var lastTime;
-    var draw = function () {
+    var draw = function() {
         //update time
         var now = Date.now();
         if (lastTime) {
@@ -390,12 +449,12 @@
 
     module.exports = {
         started: false,
-        startGame: function (wssClients) {
+        startGame: function(wssClients) {
             clients = wssClients;
             this.started = true;
             draw();
         },
-        handleMessage: function (message, ws) {
+        handleMessage: function(message, ws) {
             var msgObj = JSON.parse(message);
             if (msgObj.type === 'event') {
                 if (msgObj.event === 'click' && playerMap[ws.playerId] !== undefined) {
@@ -435,12 +494,12 @@
                 }
             }
         },
-        onPlayerConnect: function (ws) {
+        onPlayerConnect: function(ws) {
             addNewPlayer(ws);
             console.log('player connected: ' + ws.playerId);
 
         },
-        onPlayerDisconnect: function (ws) {
+        onPlayerDisconnect: function(ws) {
             console.log('player disconnected: ' + ws.playerId);
             if (gunMap[ws.playerId]) {
                 delete entities[gunMap[ws.playerId].id];
--- myjs.js
+++ myjs.js
@@ -1,80 +1,103 @@
 /*global console, $, window, WebSocket, setInterval, document*/
-(function () {
+(function() {
     'use strict';
-    var setCanvasSize = function (ctx) {
+
+    var PIXEL_RATIO = (function() {
+        var ctx = document.createElement("canvas").getContext("2d"),
+            dpr = window.devicePixelRatio || 1,
+            bsr = ctx.webkitBackingStorePixelRatio || ctx.mozBackingStorePixelRatio || ctx.msBackingStorePixelRatio || ctx.oBackingStorePixelRatio || ctx.backingStorePixelRatio || 1;
+
+        return dpr / bsr;
+    }());
+
+
+    var createHiDPICanvas = function(w, h, ratio) {
+        if (!ratio) {
+            ratio = PIXEL_RATIO;
+        }
+        var can = document.createElement("canvas");
+        can.width = w * ratio;
+        can.height = h * ratio;
+        can.style.width = w + "px";
+        can.style.height = h + "px";
+        can.getContext("2d").setTransform(ratio, 0, 0, ratio, 0, 0);
+        return can;
+    };
+
+    var createCanvas = function(container) {
         //TODO: fix this
-        var container = $("body");
 
-        var width = ctx.canvas.width;
-        var height = ctx.canvas.height;
-        var maxWidth = container.width();
-        var maxHeight = container.height();
-
+        var width = 1920;
+        var height = 1080;
+        var maxWidth = container.width() - 10;
+        var maxHeight = container.height() - 10;
 
         var ratio = maxWidth / width;
         if (height * ratio > maxHeight) {
             ratio = maxHeight / height;
         }
 
-        ctx.canvas.width = (width * ratio);
-        ctx.canvas.height = (height * ratio);
-
-        // console.log(ctx.canvas.width, ctx.canvas.height);
+        var canvas = createHiDPICanvas(width, height, 1.5);
+        container.append(canvas);
+        return canvas;
     };
 
-    window.onload = function () {
+    window.onload = function() {
         var body = $('body');
-        var canvas = $('#canvas');
+        // var canvas = $('#canvas');
+        // var ctx = canvas[0].getContext('2d');
+
+        var canvas = $(createCanvas(body));
+        canvas.css('border', '1px solid black');
         var ctx = canvas[0].getContext('2d');
 
-        setCanvasSize(ctx);
-        $(window).resize(function () {
-            setCanvasSize(ctx);
-        });
+        // $(window).resize(function() {
+        //     setCanvasSize(ctx);
+        // });
         var entities = {};
         var player;
         var mouseX;
         var mouseY;
         var websocket;
 
-        var start = function (websocketServerLocation) {
+        var start = function(websocketServerLocation) {
             if (websocket) {
                 websocket.close();
                 // delete websocket;
             }
             websocket = new WebSocket(websocketServerLocation);
-            websocket.onopen = function () {
+            websocket.onopen = function() {
                 if (window.timerID) {
                     window.clearInterval(window.timerID);
                     window.timerID = 0;
                 }
                 console.log('websocket opened');
             };
-            websocket.onclose = function () {
+            websocket.onclose = function() {
                 console.log('websocket closed');
                 if (!window.timerID) {
-                    window.timerID = setInterval(function () {
+                    window.timerID = setInterval(function() {
                         start(websocketServerLocation);
                     }, 2000);
                 }
             };
 
             var obj;
-            websocket.onmessage = function (m) {
+            websocket.onmessage = function(m) {
                 obj = JSON.parse(m.data);
                 entities = obj.entities;
                 player = obj.player;
                 // console.log('recieved: ', m);
             };
 
-            var send = function (obj) {
+            var send = function(obj) {
                 // console.log('sending: ', obj);
                 if (websocket.readyState === 1) {
                     websocket.send(JSON.stringify(obj));
                 }
             };
 
-            canvas.click(function (e) {
+            canvas.click(function(e) {
                 var rect = ctx.canvas.getBoundingClientRect();
                 mouseX = e.clientX - rect.left;
                 mouseY = e.clientY - rect.top;
@@ -87,7 +110,7 @@
                 });
             });
 
-            body.mousemove(function (e) {
+            body.mousemove(function(e) {
                 e.preventDefault();
                 e.stopImmediatePropagation();
                 var rect = ctx.canvas.getBoundingClientRect();
@@ -108,13 +131,13 @@
                 }
             });
 
-            canvas.contextmenu(function (e) {
+            canvas.contextmenu(function(e) {
                 e.preventDefault();
                 e.stopImmediatePropagation();
                 //right click....maybe do sumthin here?
             });
 
-            window.addEventListener('keydown', function (e) {
+            window.addEventListener('keydown', function(e) {
                 var rect = ctx.canvas.getBoundingClientRect();
                 var code = e.keyCode;
                 if (code === 82) {
@@ -130,7 +153,7 @@
                 } else {
                     e.preventDefault();
                     e.stopImmediatePropagation();
-                    var checkCode = function (a, b, direction) {
+                    var checkCode = function(a, b, direction) {
                         if ((code === a || code === b) && !(mouseX < rect.left || mouseX > rect.width || mouseY < rect.top || mouseY > rect.height)) {
                             send({
                                 type: 'event',
@@ -148,11 +171,9 @@
                 }
             });
 
-            window.addEventListener('keyup', function (e) {
-                e.preventDefault();
-                e.stopImmediatePropagation();
+            window.addEventListener('keyup', function(e) {
                 var code = e.keyCode;
-                var checkCode = function (a, b, direction) {
+                var checkCode = function(a, b, direction) {
                     if (code === a || code === b) {
                         send({
                             type: 'event',
@@ -167,66 +188,87 @@
                 checkCode(40, 83, 'down');
             });
 
-            //game loop
-            var draw = function () {
-                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            var getEntityColor = function(e) {
+                var color;
+                if (typeof e.color === 'string') {
+                    color = e.color;
+                } else if (typeof e.color === 'object') {
+                    color = "rgba(" + e.color.r + ", " + e.color.g + ", " + e.color.b + ", " + e.color.a || 1 + ")";
+                } else {
+                    color = 'black';
+                }
+                return color;
+            };
+
+            var drawType = function(type) {
                 var e;
                 for (e in entities) {
                     if (entities[e] !== null) {
-                        if (entities[e].shape === 'circle' && !entities[e].isDead) {
-                            ctx.beginPath();
-                            ctx.arc(entities[e].x, entities[e].y, entities[e].r, 0, 2 * Math.PI, false);
-                            if (typeof entities[e].color === 'string') {
-                                ctx.fillStyle = entities[e].color;
-                            } else if (typeof entities[e].color === 'object') {
-                                ctx.fillStyle = "rgba(" + entities[e].color.r + ", " + entities[e].color.g + ", " + entities[e].color.b + ", " + entities[e].color.a || 1 + ")";
-                            } else {
-                                ctx.fillStyle = 'black';
+                        if (entities[e].type === type) {
+                            if (entities[e].shape === 'circle' && !entities[e].isDead) {
+                                ctx.beginPath();
+                                ctx.fillStyle = getEntityColor(entities[e]);
+                                ctx.arc(entities[e].x , entities[e].y , entities[e].r, 0, 2 * Math.PI , false);
+                                ctx.fill();
+                            } else if (entities[e].shape === 'rectangle') {
+                                ctx.beginPath();
+                                ctx.fillStyle = getEntityColor(entities[e]);
+                                ctx.rect(entities[e].x , entities[e].y , entities[e].w , entities[e].h );
+                                ctx.fill();
+                            } else if (entities[e].shape === 'line') {
+                                ctx.beginPath();
+                                ctx.moveTo(entities[e].x1 , entities[e].y1 );
+                                ctx.lineTo(entities[e].x2 , entities[e].y2 );
+                                ctx.lineWidth = 3 ;
+                                ctx.strokeStyle = 'black';
+                                ctx.stroke();
                             }
-                            ctx.fill();
-                        } else if (entities[e].shape === 'line') {
-                            ctx.beginPath();
-                            ctx.moveTo(entities[e].x1, entities[e].y1);
-                            ctx.lineTo(entities[e].x2, entities[e].y2);
-                            ctx.lineWidth = 3;
-                            ctx.strokeStyle = 'black';
-                            ctx.stroke();
-                        }
 
-                        // add stroke to current player
-                        if (player && player.id === entities[e].id) {
-                            ctx.lineWidth = 3;
-                            ctx.strokeStyle = 'black';
-                            ctx.stroke();
+                            // add stroke to current player
+                            if (player && player.id === entities[e].id) {
+                                ctx.beginPath();
+                                ctx.lineWidth = 3 ;
+                                ctx.strokeStyle = 'black';
+                                ctx.stroke();
+                            }
                         }
                     }
                 }
+            };
+
+            //game loop
+            var draw = function() {
+                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+                drawType('thing');
+                drawType('bullet');
+                drawType('gun');
+                drawType('player');
 
                 if (player) {
                     ctx.font = "18px serif";
                     ctx.strokeStyle = 'white';
                     ctx.fillStyle = 'black';
-                    ctx.strokeText('Ammo: ' + player.ammo + '/' + player.maxAmmo, 10.5, 30.5);
-                    ctx.fillText('Ammo: ' + player.ammo + '/' + player.maxAmmo, 10.5, 30.5);
+                    ctx.strokeText('Ammo: ' + player.ammo + '/' + player.maxAmmo, 10.5 , 30.5 );
+                    ctx.fillText('Ammo: ' + player.ammo + '/' + player.maxAmmo, 10.5 , 30.5 );
                     if (player.reloadPercentage) {
-                        ctx.strokeText(player.reloadPercentage + '% reloaded...', 120.5, 30.5);
-                        ctx.fillText(player.reloadPercentage + '% reloaded...', 120.5, 30.5);
+                        ctx.strokeText(player.reloadPercentage + '% reloaded...', 120.5 , 30.5 );
+                        ctx.fillText(player.reloadPercentage + '% reloaded...', 120.5 , 30.5 );
                     } else if (player.ammo === 0) {
-                        ctx.strokeText('Press R to reload!', 120.5, 30.5);
-                        ctx.fillText('Press R to reload!', 120.5, 30.5);
+                        ctx.strokeText('Press R to reload!', 120.5 , 30.5 );
+                        ctx.fillText('Press R to reload!', 120.5 , 30.5 );
                     }
 
-                    console.log(player.kills, player.deaths);
-                    ctx.strokeText('Kills: ' + player.kills, 10.5, 60.5);
-                    ctx.fillText('Kills: ' + player.kills, 10.5, 60.5);
-                    ctx.strokeText('Deaths: ' + player.deaths, 10.5, 90.5);
-                    ctx.fillText('Deaths: ' + player.deaths, 10.5, 90.5);
-                    ctx.strokeText('Health: ' + player.healthPercentage + '%', 10.5, 120.5);
-                    ctx.fillText('Health: ' + player.healthPercentage + '%', 10.5, 120.5);
+                    ctx.strokeText('Kills: ' + player.kills, 10.5 , 60.5 );
+                    ctx.fillText('Kills: ' + player.kills, 10.5 , 60.5 );
+                    ctx.strokeText('Deaths: ' + player.deaths, 10.5 , 90.5 );
+                    ctx.fillText('Deaths: ' + player.deaths, 10.5 , 90.5 );
+                    ctx.strokeText('Health: ' + player.healthPercentage + '%', 10.5 , 120.5 );
+                    ctx.fillText('Health: ' + player.healthPercentage + '%', 10.5 , 120.5 );
 
                     if (player.isDead) {
-                        ctx.strokeText('Press Spacebar to respawn!', 10.5, 150.5);
-                        ctx.fillText('Press Spacebar to respawn!', 10.5, 150.5);
+                        ctx.strokeText('Press Spacebar to respawn!', 10.5 , 150.5 );
+                        ctx.fillText('Press Spacebar to respawn!', 10.5 , 150.5 );
                     } else {
                         //show health
                     }
